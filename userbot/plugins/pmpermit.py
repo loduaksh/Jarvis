fde7c1706d5477b3832574adc41657e4242a4915
Jay Parmar <55135328+japarmar@users.noreply.github.com>
2020-05-02 20:03:06 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -54,6 +54,21 @@ if Var.PRIVATE_GROUP_ID is not None:
                 await asyncio.sleep(3)
                 await event.client(functions.contacts.BlockRequest(chat.id))
 
+    @command(pattern="^.disapprove ?(.*)")
+    async def approve_p_m(event):
+        if event.fwd_from:
+            return
+        replied_user = await event.client(GetFullUserRequest(event.chat_id))
+        firstname = replied_user.user.first_name
+        reason = event.pattern_match.group(1)
+        chat = await event.get_chat()
+        if event.is_private:
+            if chat.id == 586949777:
+              await event.edit("Sorry, I Can't Disapprove My Master")
+            else:  
+              if pmpermit_sql.is_approved(chat.id):
+                  pmpermit_sql.disapprove(chat.id)
+                  await event.edit("Disapproved to pm [{}](tg://user?id={})".format(firstname, chat.id))
 
     @command(pattern="^.listapproved")
     async def approve_p_m(event):
