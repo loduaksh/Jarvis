2f982db64f1252a9ff9cb0e4ac102ffab7afc5da
neerajregmi10 <62173618+neerajregmi10@users.noreply.github.com>
2020-05-14 18:18:56 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,4 +1,4 @@
-"""Check if userbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
+"""Check if userbot alive."""
 import asyncio
 from telethon import events
 from telethon.tl.types import ChannelParticipantsAdmins
@@ -11,8 +11,8 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars 
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("`Zinda Hu Sirr ^.^ \nYour bot is running\n\nTelethon version: 6.9.0\nPython: 3.7.3\n\n`"
+    await alive.edit("`**Jarvis at your service !!** ^.^ \nYour bot is running\n\nTelethon version: 6.9.0\nPython: 3.7.3\n\n`"
                      f"`My peru owner`: {DEFAULTUSER}\n"
                      "`Telethon version: 6.9.0\nPython: 3.7.3\n`\n"
-                     "`Database Status: Databases functioning normally!\n\nAlways with you, my master!\n`"
+                     "`Database Status: Databases functioning normally!\nAlways with you, my master!\n`"
                      "")
