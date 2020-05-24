54344863cf4ff3eb64deff9b22881899f4f042d8
Jay Parmar <55135328+japarmar@users.noreply.github.com>
2020-05-24 12:57:40 +0530

    Update app.py

--- userbot/plugins/app.py
+++ userbot/plugins/app.py
@@ -68,7 +68,7 @@ async def apk(e):
 
         app_details += "\n<code>Features :</code> <a href='"+app_link+"'>View in Play Store</a>"
 
-        app_details += "\n\n===> @Gujju_Bot_Support <==="
+        app_details += "\n\n===> [Jarvis](https://t.me/joinchat/IvwkkR0eoeU6qchMUToJxA) <==="
 
         await e.edit(app_details, link_preview = True, parse_mode = 'HTML')
 
@@ -126,7 +126,7 @@ async def apkr(e):
 
         app_details += "\n\n<b>Download : </b> <a href='https://t.me/IndianBot_Official'>Request_Here by typing #request</a>"
 
-        app_details += "\n\n===> @Gujju_Bot_Support <==="
+        app_details += "\n\n===> [Jarvis](https://t.me/joinchat/IvwkkR0eoeU6qchMUToJxA) <==="
 
         await e.edit(app_details, link_preview = True, parse_mode = 'HTML')
 
